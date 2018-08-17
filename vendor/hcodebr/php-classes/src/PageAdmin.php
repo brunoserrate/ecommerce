121789d30ae2792776f61a33386460220a6663ef
Bruno Serrate <brunostj@hotmail.com>
2018-08-17 16:54:34 -0300

    remoção de arquivos de downloads

---
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace Hcode;
+
+class PageAdmin extends Page {
+
+	public function __construct($opt = array(), $tpl_dir = "/views/admin/"){
+
+		parent::__construct($opt, $tpl_dir);
+	}
+
+
+}
+
+ ?>
